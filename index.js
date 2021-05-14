8da3a39e262140d1ffa0740bafbeb6aad03c96ae
nickfrahm <nfrahm24@gmail.com>
2021-05-13 20:47:12 -0500

    basic game logic

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
+//on load, create both players, default 1 to user & 2 to CPU
+let playerX;
+let playerO;
+
+window.addEventListener('load', () => {
+    playerX = playerFactory("Player 1", "X", true);
+    playerO = playerFactory("Computer", "O", false);
+  });
+
+//add click listener--> main driver for game logic.
+const boardTiles = document.querySelectorAll(".gameBoard__tile");
+boardTiles.forEach((tile) => {
+    tile.addEventListener("click", () => {
+        //on click, try and play a square and check winner
+        if (game.getTilesMarked() <9) {
+            if (game.checkTurn() === "X") {
+                tile.firstChild.innerHTML = playerX.marker;
+                const sq = gameBoard.determineSquare(tile.id.toString());
+                gameBoard.setBoard(sq[0], sq[1], playerX.marker);
+            } else {
+                tile.firstChild.innerHTML = playerO.marker;
+                const sq = gameBoard.determineSquare(tile.id.toString());
+                gameBoard.setBoard(sq[0], sq[1], playerO.marker);
+            }
+            //console.log(gameBoard.board)
+            game.checkWinner();
+        }
+    });
+})
+
+//player factory
+const playerFactory = (name, marker, isHuman) => {
+    const getName = () => {
+        return name;
+    }
+    
+    const changeName = (newName) => {
+        name = newName;
+    }
+
+    return {changeName, getName, marker};
+}
+
+//board module pattern
 const gameBoard = (() => {
     const board = [
         ["", "", ""],
         ["", "", ""],
         ["", "", ""]
     ]
-    const setBoard = (row, column, letter) => {
-        board[row][column] = letter;
+    const setBoard = (row, column, marker) => {
+        board[row][column] = marker;
     }
     const clearBoard = () => {
         for (let i = 0; i < board.length; i++) {
@@ -14,20 +58,52 @@ const gameBoard = (() => {
             }
         }
     }
-    return {setBoard, clearBoard, board};
+    const determineSquare = (tileId) => {
+        //breakdown the id into which column and row and return in an array
+        //used for sending into setBoard;
+        let rowCol = [];
+        switch (tileId[0]) {
+            case "t": 
+              rowCol[0] = 0;
+              break;
+            case "m": 
+              rowCol[0] = 1;
+              break;
+            case "b": 
+              rowCol[0] = 2;
+              break;
+        }
+        switch (tileId[1]) {
+            case "l": 
+              rowCol[1] = 0;
+              break;
+            case "m": 
+              rowCol[1] = 1;
+              break;
+            case "r": 
+              rowCol[1] = 2;
+              break;
+        }
+        return rowCol;
+    }
+    return {setBoard, clearBoard, determineSquare, board};
 })();
 
+//game module
 const game = (() => {
     let scoreX = 0;
     let scoreO = 0;
+    let tilesMarked = 0;
 
-    const increaseX = () => {
-        scoreX++;
+    const getTilesMarked = () => {
+        return tilesMarked;
     }
 
-    const increaseO = () => {
-        scoreO++;
-    }
+    const increaseTilesMarked = () => tilesMarked++;
+
+    const increaseX = () => scoreX++;
+
+    const increaseO = () => scoreO++;
 
     const getScores = () => {
         return [scoreX, scoreO];
@@ -38,5 +114,28 @@ const game = (() => {
         scoreO = 0;
     }
 
-    return {increaseX, increaseO, getScores, resetScores}
+    const checkWinner = () => {
+        increaseTilesMarked();
+        if (checkRowW() || checkColW() || checkDiagW()) {
+
+        }
+    }
+
+    const checkTurn = () => {
+        if (tilesMarked % 2 === 0) {
+            return "X";
+        }
+        return "O";
+    }
+
+    const checkRowW = () => {
+
+    }
+    const checkColW = () => {
+
+    }
+    const checkDiagW = () => {
+
+    }
+    return {increaseX, increaseO, getScores, resetScores, checkTurn, checkWinner, getTilesMarked}
 })();
